619566c05b518106938ed7097b02c339a4146602
Júnior <Babilonia364@users.noreply.github.com>
2018-11-11 14:14:03 -0200

    Atualizando socket para inteiros
    Socket envia inteiros bem como calcula seu tempo de comunicação.

--- Socket/socket1_1.c
+++ Socket/socket1_1.c
@@ -5,86 +5,110 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#define PORT 8080 
+#include <arpa/inet.h>
+#include <fcntl.h>									/*				O_CREAT, O_EXEC					*/
+#include <stdint.h>									/*					int32_t						*/
+#include <time.h>									/*				clock_t, clock()				*/
+#include <sys/wait.h>								/*				wait, waitpid					*/
+
+
+#define PORT 8080
+#define BEGIN 9999
 
 
 int main(int argc, char const *argv[])
 {
 	pid_t pid;
-
+	clock_t tempo1, tempo2;
+	
+	struct sockaddr_in address;
+	int32_t buffer[3], num[3], soma;
+	
+	tempo1=clock();
 	if((pid=fork())<0)
 	{
 		perror("fork");
 		exit(1);
 	}
-
+	
+	
+	/* Servidor do socket */
 	if(pid>0)
 	{
 		int server_fd, new_socket, valread; 
-	    	struct sockaddr_in address; 
-    	int opt = 1; 
-    	int addrlen = sizeof(address); 
-    	char buffer[1024] = {0}; 
-    	char *hello = "Hello from server"; 
-       
-    	// Creating socket file descriptor 
-    	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    	{ 
-        	perror("socket failed"); 
-        	exit(EXIT_FAILURE); 
-    	} 
-       
-    	// Forcefully attaching socket to the port 8080 
-    	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    	{ 
-        	perror("setsockopt"); 
-        	exit(EXIT_FAILURE); 
-    	} 
-    	address.sin_family = AF_INET; 
-    	address.sin_addr.s_addr = INADDR_ANY; 
-    	address.sin_port = htons( PORT ); 
-       
-    	// Forcefully attaching socket to the port 8080 
-    	if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    	{ 
-        	perror("bind failed"); 
-        	exit(EXIT_FAILURE); 
-    	} 
-    	if (listen(server_fd, 3) < 0) 
-    	{ 
-        	perror("listen"); 
-        	exit(EXIT_FAILURE); 
-    	} 
-    	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    	{ 
-        	perror("accept"); 
-        	exit(EXIT_FAILURE); 
-    	} 
-    	valread = read( new_socket , buffer, 1024); 
-    	printf("%s\n",buffer ); 
-    	send(new_socket , hello , strlen(hello) , 0 ); 
-    	printf("Hello message sent\n");
-
-
+		int opt = 1; 
+		int addrlen = sizeof(address);
+		
+		
+		// Creating socket file descriptor 
+		if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+		{ 
+			perror("socket failed"); 
+			exit(EXIT_FAILURE);
+		} 
+		
+		// Forcefully attaching socket to the port 8080 
+		if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+													&opt, sizeof(opt))) 
+		{ 
+			perror("setsockopt"); 
+			exit(EXIT_FAILURE); 
+		} 
+		address.sin_family = AF_INET; 
+		address.sin_addr.s_addr = INADDR_ANY; 
+		address.sin_port = htons( PORT ); 
+		
+		// Forcefully attaching socket to the port 8080 
+		if (bind(server_fd, (struct sockaddr *)&address,  
+									sizeof(address))<0) 
+		{ 
+			perror("bind failed"); 
+			exit(EXIT_FAILURE); 
+		}
+		
+		/* Dando listen, esperando por conexoes no socket ja criado */
+		printf("Servidor dando listen\n");
+		if (listen(server_fd, 3) < 0) 
+		{ 
+			perror("listen"); 
+			exit(EXIT_FAILURE); 
+		} 
+		
+		
+		/* Extrai a primeira conexao de conexoes pendentes na fila */
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+						(socklen_t*)&addrlen))<0) 
+		{ 
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		}
+			valread = read( new_socket , buffer, 3*sizeof(int)); 						/* Recebendo mensagem do cliente */
+			printf("buffer: %d\n", buffer[0]);
+		printf("buffer[0]: %d\nbuffer[1]: %d\n", buffer[0], buffer[1]);
+		num[0]=buffer[0]+buffer[1];
+		send(new_socket , num , (sizeof(num)) , 0 );					/* Enviando mensagem de volta para o cliente */
+		printf("Soma enviada\n");
+		
+		wait(NULL);
+		tempo2=clock();
+		printf("Tempo: %lf\n", (tempo2 - tempo1)/(double)CLOCKS_PER_SEC);
+		
 	}
-	else if(pid==0)
+	
+	/* Cliente do socket */
+	else
 	{
-		struct sockaddr_in address; 
 	int sock = 0, valread; 
 	struct sockaddr_in serv_addr; 
-	char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
+	
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{ 
 		printf("\n Socket creation error \n"); 
 		return -1; 
 	} 
-
+	
 	memset(&serv_addr, '0', sizeof(serv_addr)); 
-
+	
 	serv_addr.sin_family = AF_INET; 
 	serv_addr.sin_port = htons(PORT); 
 	
@@ -94,15 +118,22 @@ int main(int argc, char const *argv[])
 		printf("\nInvalid address/ Address not supported \n"); 
 		return -1; 
 	} 
-
+	
+	/* Definindo numeros */
+	num[0]=2;
+	num[1]=1;
+	printf("Numeros a serem somados %d + %d\n", num[0], num[1]);
+	
+	/* Conecta o socket */
+	printf("Clente conectando\n");
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
 	{ 
 		printf("\nConnection Failed \n"); 
 		return -1; 
 	} 
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	valread = read( sock , buffer, 1024); 
-	printf("%s\n",buffer );
+	send(sock , num , (sizeof(num)) , 0 ); 				/* Enviando mensagem para o servidor */
+	printf("Numeros enviados\n"); 
+	valread = read( sock , buffer, sizeof(int)); 						/* Recebendo mensagem do servidor */
+	printf("Soma: %d\n",buffer[0] );
 	}
 }
